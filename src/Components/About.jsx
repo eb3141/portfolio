fb24ff8bd6dc2beff1e272cb628dd518dfa2844c
eb3141 <elliott_barthel@outlook.com>
2025-08-07 20:18:25 +0000

    change for deployment to GitHub Pages

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "/workspaces/portfolio/src/images/landscape.jpg";
+import image from "/workspaces/portfolio/src/images/design-desk.jpeg";
 
 const imageAltText = "two males military";
 
